e165088e7bab8a3c617a2a84297ca8ad2beeb582
Gaurav Manek <gaurav@gauravmanek.com>
2019-08-20 19:41:56 -0400

    Changed setup package name.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="autolab-15388",
+    name="autolab-testing",
     version="0.0.1",
     author="Gaurav Manek",
     author_email="gaurav@gauravmanek.com",
